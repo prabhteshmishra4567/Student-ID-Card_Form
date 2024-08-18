0f3184ce996ecdb079e3d9a2c8eba760afbe0f2d
PRABHTESH MISHRA <arhimanshu4567@gmail.com>
2024-08-18 21:52:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,137 +16,39 @@ function updateCard() {
 
     document.getElementById("contactNoVal").innerText =
         document.getElementById("contact").value;
+
+    // Update the image in the card
+    const imageInput = document.getElementById('imageUpload');
+    const profileImage = document.getElementById('profileImage');
+
+    if (imageInput.files && imageInput.files[0]) {
+        const reader = new FileReader();
+
+        reader.onload = function(e) {
+            profileImage.src = e.target.result;
+        }
+
+        reader.readAsDataURL(imageInput.files[0]);
+    }
 }
-
-
 
 function clearAll() {
     // Clear input fields
     const inputs = document.querySelectorAll('input');
     inputs.forEach(input => input.value = '');
 
-    // Optionally, clear the displayed student details
+    // Reset the displayed student details
     document.getElementById("schoolNameVal").innerText = "School Name";
     document.getElementById("studentNameVal").innerText = "Devanshu";
-    document.getElementById("dateOfbirthVal").innerText = "10/05/2005";
-    document.getElementById("classVal").innerText = "2";
-    document.getElementById("rollNoVal").innerText = "3";
+    document.getElementById("dateOfbirthVal").innerText = "DD/MM/YYYY";
+    document.getElementById("classVal").innerText = "9th";
+    document.getElementById("rollNoVal").innerText = "########";
     document.getElementById("contactNoVal").innerText = "1234554321";
 
-    // document.getElementById("profileImage").src = "PFP.jpeg";
+    // Reset the profile image
+    document.getElementById('profileImage').src = './image/image.png';
 }
-
-
-
-
-
 
 function printIdcard() {
     window.print();
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function updateCard () {
-
-//    document.getElementById("schoolNameVal").innerText =
-//     document.getElementById("schoolName").value;
-  
-//    document.getElementById("studentNameVal").innerText =
-//     document.getElementById("studentName").value;
-
-//    document.getElementById("dateOfbirthVal").innerText =
-//     document.getElementById("dateOfbirth").value;
-
-//    document.getElementById("classVal").innerText =
-//     document.getElementById("class").value;
-
-//    document.getElementById("rollNoVal").innerText =
-//     document.getElementById("rollno").value;
-
-//    document.getElementById("contactNoVal").innerText =
-//     document.getElementById("contact").value;
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function clearAll () {
-
-
-// document.getElementById("schoolName").value = "";
-  
-// document.getElementById("studentName").value = "";
-
-// document.getElementById("dateOfbirth").value = "";
-
-// document.getElementById("class").value = "";
-
-// document.getElementById("rollno").value = "";
-
-// document.getElementById("contact").value = "";
-// }
